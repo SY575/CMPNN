8946b71e97ffcfba4a19caacd3d6ef37fb4d8fc6
jhxqy <1139648416@qq.com>
2023-09-17 15:29:48 +0800

    [feat] diff

--- diff.sh
+++ diff.sh
@@ -14,12 +14,21 @@ printf "%-20s %-10s %-10s %-10s %-10s\n" "Task" "Base" "Test" "Diff" "Percent"
 
 for index in "${!tasks[@]}"
 do
+    base_val=""
+    test_val=""
     base_path="$base/${tasks[$index]}/verbose.log"
     test_path="$test/${tasks[$index]}/verbose.log"
     last_base_line=$(grep -v '^$' "$base_path" | tail -n 1)
     last_test_line=$(grep -v '^$' "$test_path" | tail -n 1)
-    base_val=$(echo $last_base_line | grep -oP '(?<=auc = )\d+\.\d+')
-    test_val=$(echo $last_test_line | grep -oP '(?<=auc = )\d+\.\d+')
+    if [ "${tasks[$index]}" = "esol" ] || [ "${tasks[$index]}" = "freesolv" ]; then
+        base_val=$(echo $last_base_line | grep -oP '(?<=rmse = )\d+\.\d+')
+        test_val=$(echo $last_test_line | grep -oP '(?<=rmse = )\d+\.\d+')
+    else
+        base_val=$(echo $last_base_line | grep -oP '(?<=auc = )\d+\.\d+')
+        test_val=$(echo $last_test_line | grep -oP '(?<=auc = )\d+\.\d+')
+    fi
+    # base_val=$(echo $last_base_line | grep -oP '(?<=auc = )\d+\.\d+')
+    # test_val=$(echo $last_test_line | grep -oP '(?<=auc = )\d+\.\d+')
     difference=$(echo "$test_val - $base_val" | bc)
     percentage=$(echo "scale=2; ($difference / $base_val) * 100" | bc)
     if (( $(echo "$difference < 0" | bc -l) )); then
